60db8d5b27017bd5f0ec10c151b76662b31ecdc4
Jhonatan Scremim <jhonatanscremim@gmail.com>
2022-01-19 22:09:36 -0300

    feat(event-controller)
    Cria enpoints para controller de Eventos

--- src/ProEventos.API/Controllers/EventController.cs
+++ src/ProEventos.API/Controllers/EventController.cs
@@ -7,6 +7,8 @@ using Microsoft.Extensions.Logging;
 using ProEventos.Repository;
 using ProEventos.Domain;
 using ProEventos.Repository.Context;
+using ProEventos.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
 
 namespace ProEventos.API.Controllers
 {
@@ -14,17 +16,100 @@ namespace ProEventos.API.Controllers
     [Route("api/v1/[controller]")]
     public class EventController : ControllerBase
     {
-        private readonly DataContext _context;
+        private readonly IEventService _eventService;
 
-        public EventController(DataContext context)
+        public EventController(IEventService eventService)
         {
-            _context = context;
+            _eventService = eventService;
         }
 
         [HttpGet]
-        public IEnumerable<Event> Get()
+        public async Task<IActionResult> GetAllEvents()
         {
-            return _context.Event.ToList();
+            try{
+                return Ok(await _eventService.GetAllEventsAsync(true));
+            }
+            catch(Exception e){
+                return this.StatusCode(StatusCodes.Status500InternalServerError,
+                "Error: " + e.Message);
+            }
         }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetEventById([FromQuery] int id){
+            try{
+
+                var response = await _eventService.GetEventByIdAsync(id, true);
+
+                if(response == null)
+                    return NotFound("Event not found");
+
+                return Ok(response);
+            }
+            catch(Exception e){
+                return this.StatusCode(StatusCodes.Status500InternalServerError,
+                "Error: " + e.Message);
+            }
+        }
+
+        [HttpGet("name/{name}")]
+        public async Task<IActionResult> GetEventByName([FromQuery] string name){
+            try{
+                var response = await _eventService.GetAllEventsByNameAsync(name, true);
+
+                if(response == null)
+                    return NotFound("Events not found");
+
+                return Ok(response);
+            }
+            catch(Exception e){
+                return this.StatusCode(StatusCodes.Status500InternalServerError,
+                "Error: " + e.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateEvent([FromBody] Event model){
+            try{
+                var response = await _eventService.CreateEventAsync(model);
+
+                if(response == null)
+                    return BadRequest("Unable to register");
+
+                return Ok(response);
+            }
+            catch(Exception e){
+                return this.StatusCode(StatusCodes.Status500InternalServerError,
+                "Error: " + e.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateEvent([FromQuery] int id, [FromBody] Event model){
+            try{
+                var response = await _eventService.UpdateEventAsync(id, model);
+
+                if(response == null)
+                    return BadRequest("Unable to update");
+
+                return Ok(response);
+            }
+            catch(Exception e){
+                return this.StatusCode(StatusCodes.Status500InternalServerError,
+                "Error: " + e.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEvent([FromQuery] int id){
+            try{
+                return await _eventService.DeleteEventAsync(id) ? BadRequest("Unable to delete") : Ok("Success");
+            }
+            catch(Exception e){
+                return this.StatusCode(StatusCodes.Status500InternalServerError,
+                "Error: " + e.Message);
+            }
+        }
+
     }
 }
